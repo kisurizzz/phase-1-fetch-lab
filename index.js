72cda37b839acdd05b00acc3a4a12844601075e6
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-03 08:11:05 +0300

    initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(data));
+
 }
+fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +19,9 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
+
